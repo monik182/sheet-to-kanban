778ed7b7d43328e36ca55ba0d42bda953407d796
Monica Carvajal <monik.182@gmail.com>
2026-04-30 17:00:28 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sheet-to-kanban/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
